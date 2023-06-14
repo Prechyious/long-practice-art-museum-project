b2710995ab57b9eaf719d1d7cbb6293b9e63275e
Prechyious <lemoprechy@gmail.com>
2023-06-14 17:04:08 +0100

    Finished Phase-3

--- src/App.js
+++ src/App.js
@@ -8,8 +8,18 @@ function App() {
         <div className="page-wrapper">
             <GalleryNavigation galleries={harvardArt.records} />
             <Switch>
-                <Route exact path="/galleries/:galleryId">
+                <Route exact path="/">
+                    <h2>Harvard Art Museum</h2>
+                    <p>
+                        Look, but Don't Touch. Please select a Gallery in the
+                        navigation bar.
+                    </p>
+                </Route>
+                <Route path="/galleries/:galleryId">
                     <GalleryView galleries={harvardArt.records} />
+                </Route>
+                <Route>
+                    <h2>Page Not Found</h2>
                 </Route>
             </Switch>
         </div>
